f39d9f79cafe0c8228a37dbd2490981bbea67b13
PoojaryKeerthan <keerthanpoojary221@gmail.com>
2025-04-25 21:17:27 +0530

    updated index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,10 @@ const index = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className='text-lg'>index</Text>
+      <View>
+        <Text>hello</Text>
+      </View>
+      <Text>this is the index page of the app</Text>
     </View>
   )
 }
